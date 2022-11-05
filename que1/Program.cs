e4ad2c1fc36265b877dd270972718946628c3808
yung-timz <ogungbayitimilehin3@gmail.com>
2022-11-04 13:01:37 -1200

    first commit

---
+++ que1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace que1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int[] nums = {2,7,9,8,4,9,4,2};
+              int original = 4;
+             Console.WriteLine(CheckNumber(nums,original));
+        }  
+        public static int CheckNumber(int[] nums, int original)
+        {
+            for(int a = 0; a < nums.Length; a++)
+            {
+                if(original == nums[a])
+                {
+                    original = 2 * original;
+                }
+            }
+            return original;
+        }
+                                
+    }
+}  
